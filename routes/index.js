135e378b0dc3943744f3f045a84f219f6bb87ffc
lava-git <105036972+lava-git@users.noreply.github.com>
2023-02-24 15:16:09 +0100

    database added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
+
+function getExperiences(req, res) {
+
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Express' });
+});
+
+// GET all the experiences without a filter
+router.get('/experiences', function(req, res, next) {
+  db(`SELECT * FROM experiences`)
+});
+
+// GET all the experiences with filter category backpacker
+router.get('/experiences/:category', function(req, res, next) {
+  db(`SELECT * FROM experiences WHERE category=${req.params.categoryID}`)
+  //TODO req.query.category (question mark) or req.params.category (with the colons) -- THINK OF IT LIKE THE ID
+});
+
+// GET the about page (stil need the slash)
+router.get('/about', function(req, res, next) {
+  
+});
+
+// GET the contact page
+router.get('/contact', function(req, res, next) {
+    
 });
 
 module.exports = router;
