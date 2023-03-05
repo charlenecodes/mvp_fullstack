a9ad9594b362ad0377253ead9fc659653e7470b0
lava-git <105036972+lava-git@users.noreply.github.com>
2023-03-05 11:14:05 +0100

    Updates

---
+++ precache-manifest.b0c971866228749a9b5932be783f3606.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdc055029df4a6325749",
+    "url": "/mvp_fullstack/static/css/main.6ee66373.chunk.css"
+  },
+  {
+    "revision": "cdc055029df4a6325749",
+    "url": "/mvp_fullstack/static/js/main.381894da.chunk.js"
+  },
+  {
+    "revision": "5b18d5ee47c715e695b1",
+    "url": "/mvp_fullstack/static/js/runtime~main.070400c0.js"
+  },
+  {
+    "revision": "cc7888db3d22eebec7e1",
+    "url": "/mvp_fullstack/static/js/2.c970ba30.chunk.js"
+  },
+  {
+    "revision": "617237940f1e49fa4db5f7e25fdddaaf",
+    "url": "/mvp_fullstack/index.html"
+  }
+];
